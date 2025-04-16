3fcb4a6c34db14b1e15b3d16df5c8259fb711a91
zanzungg <nvdntp2004@gmail.com>
2025-04-16 13:42:29 +0700

    First commit

---
+++ myweb/myapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('base/', views.base, name='base'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('animal/', views.animal, name='animal'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('error/', views.error, name='error'),
+    path('contact/', views.contact, name='contact'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+]
---
+++ myweb/myapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'myapp/index.html')
+
+def base(request):
+    return render(request,'myapp/base.html')
+
+def about(request):
+    return render(request,'myapp/about.html')
+
+def service(request):
+    return render(request,'myapp/service.html')
+
+def animal(request):
+    return render(request,'myapp/animal.html')
+
+def testimonial(request):
+    return render(request,'myapp/testimonial.html')
+
+def error(request):
+    return render(request,'myapp/404.html')
+
+def contact(request):
+    return render(request,'myapp/contact.html')
+
+def signin(request):
+    return render(request,'myapp/signin.html')
+
+def signup(request):
+    return render(request,'myapp/signup.html')
